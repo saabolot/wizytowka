93b96f60d3d7f49bd10ad3a148ced3847a12b550
saabolot <97003103+saabolot@users.noreply.github.com>
2022-01-20 21:01:49 +0100

    rev03

--- wizytowka.py
+++ wizytowka.py
@@ -1,31 +1,34 @@
-
-class card:  
-    #pass
-    class card():
-        def __init__(self, name, company, job, address, phone_number, email):
-            self.name = None
-            self.company = None
-            self.job = None
-            self.email = None
-            self.phone_number = None
-    class BaseContact(card):
-            def __init__(self, phone_number, address):
-                super().__init__(self, phone_number, address)
-                self.phone_number = phone_number            
-            def __contact__(self, phone_number, name):
-                self.phone_number = phone_number  
-                print(f'Wybieram numer {phone_number} i dzwonię do {name}')
-    class BusinessContact(card):
-            def __init__(self, phone_number, job, company):
-                super().__init__(self, phone_number, job, company)
-                self.phone_number = phone_number
-            def __contact__(self, phone_number, name):
-                print(f'Wybieram numer {phone_number} i dzwonię do {name}')
 from faker import Faker
+
+
+class card:
+    def __init__(self, name, phone_number, address):
+        self.name = None
+        self.email = None
+        self.phone_number = None
+
+
+class BaseContact(card):
+    def __init__(self, *args, **kwargs):
+        super().__init__(self)
+        self.phone_number = None
+
+    def __contact__(self, phone_number, name):
+        self.phone_number = phone_number
+        print(f'Wybieram numer {phone_number} i dzwonię do {name}')
+
+
+class BusinessContact(card):
+    def __init__(self, job, company, email,  *args, **kwargs):
+        super().__init__(self, job, company)
+        self.phone_number = None
+
+    def __contact__(self, phone_number, name):
+        print(f'Wybieram numer {phone_number} i dzwonię do {name}')
+
+
 fake = Faker(['pl_PL'])
 
 
-    
-person1 = BaseContact(fake.address)
+person1 = BaseContact(fake)
 print(person1)
-
